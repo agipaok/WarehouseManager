5c2100d88d7be3ee81f6a8bb5d0f27aa5bdd1cf7
agipaok <archelaos.chatzinikolaou@gmail.com>
2026-03-27 13:30:19 +0200

    CSV UPLOAD

---
+++ WarehouseManager.Maui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace WarehouseManager.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "WarehouseManager.Maui" };
+	}
+}
---
+++ WarehouseManager.Maui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseManager.Maui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
