be2b96f732ce0b52dcb7bcac2bfb5b3924b0ac4b
agipaok <158823680+agipaok@users.noreply.github.com>
2026-04-02 08:47:40 +0300

    general fixes

--- WarehouseManager/Program.cs
+++ WarehouseManager/Program.cs
@@ -16,17 +16,17 @@ builder.Services.AddScoped<StockMovementService>();
 builder.Services.AddScoped<NginxLogService>();
 builder.Services.AddMudServices();
 
-// Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
     app.UseHsts();
 }
 
